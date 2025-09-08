6f1ff132a30610b692ea376847ffc90789087622
Abigiya Getachew <ebbabi98@gmail.com>
2025-09-08 11:21:04 +0300

    Fix CORS for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  (import.meta.env.PROD ? 'https://improved-food-delivery.onrender.com/api' : 'http://localhost:5000/api');
+giconst API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
+  (import.meta.env.PROD ? 'https://food-delivery-y96l.onrender.com/api' : 'http://localhost:5000/api');
 
 export default API_BASE_URL;
